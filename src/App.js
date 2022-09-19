fb31afe7845f5c427c4f2e0a6091ef55cd63d7bd
Rajpurohit Dhanpal <rajpurohitdhanpal@gmail.com>
2022-09-19 22:19:17 +0530

    updating template

--- src/App.js
+++ src/App.js
@@ -11,17 +11,21 @@ import Index from './pages/Index';
 import Contact from './pages/Contact';
 import Blog from './pages/Blog';
 import Jobs from './pages/Jobs';
+import Header from './container/Header';
+import Footer from './container/Footer';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+      <Header/>
       <Routes>
           <Route path='/' element={<Index/>} />
           <Route path='/contact' element={<Contact/>} />
           <Route path='/blog' element={<Blog/>} />
           <Route path='/jobs' element={<Jobs/>} />
       </Routes>
+      <Footer/>
       </BrowserRouter>
     </div>
   );
--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,7 +1,5 @@
 import React from 'react'
 
-import Header from "../container/Header";
-import Footer from "../container/Footer";
 import Slider from "../container/Slider"
 import JobSearch from "../container/JobSearch";
 import JObCategories from "../container/JobCategories"; 
@@ -11,14 +9,12 @@ import CompaniesList from  "../container/CompaniesList";
 function Index() {
   return (
     <div>
-      <Header />
       <Slider />
       <JobSearch />
       <JObCategories />
       <JobList />
       <CandidateList />
       <CompaniesList />
-      <Footer />
     </div>
   )
 }
